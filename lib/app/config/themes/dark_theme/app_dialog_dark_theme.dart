04efdc7fed99a48fa89c00da62544b748ed21861
Hwassef <hassine.wassef@outloook.com>
2022-11-01 12:58:21 -1100

    Define application dialog dark theme

---
+++ lib/app/config/themes/dark_theme/app_dialog_dark_theme.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+import '../../colors/app_dark_mode_colors.dart';
+import '../../constants/app_constants.dart';
+
+class AppDialogLightTheme {
+  AppDialogLightTheme._();
+  static const DialogTheme dialogTheme = DialogTheme(
+    backgroundColor: AppDarkModeColors.darkBlueColor,
+    elevation: 0.0,
+    shape: RoundedRectangleBorder(
+      borderRadius: AppConstants.smallBorderRadius,
+      side: BorderSide(
+        color: AppDarkModeColors.whiteColor,
+        width: 1,
+      ),
+    ),
+  );
+}
