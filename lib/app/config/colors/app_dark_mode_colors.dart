13d1786b1b8667c31396d60e547fce109e42c94c
Hwassef <hassine.wassef@outloook.com>
2022-11-01 12:56:31 -1100

    Add white hex color

--- lib/app/config/colors/app_dark_mode_colors.dart
+++ lib/app/config/colors/app_dark_mode_colors.dart
@@ -3,4 +3,5 @@ import 'package:flutter/material.dart';
 class AppDarkModeColors {
   AppDarkModeColors._();
   static const Color darkBlueColor = Color(0xFF040720);
+  static const Color whiteColor = Color(0xffffffff);
 }
