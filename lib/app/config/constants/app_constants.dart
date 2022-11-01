75a38821c9b6c661be03c87c4c617f8ff175cf13
Hwassef <hassine.wassef@outloook.com>
2022-11-01 12:13:30 -1100

    Add border radius constant

---
+++ lib/app/config/constants/app_constants.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/cupertino.dart';
+
+class AppConstants {
+  AppConstants._();
+  static const double smallRadiusValue = 4.0;
+  static const Radius smallRadius = Radius.circular(smallRadiusValue);
+  static const BorderRadius smallBorderRadius = BorderRadius.all(smallRadius);
+}
