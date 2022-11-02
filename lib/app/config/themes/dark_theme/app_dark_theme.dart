61c3ce144b67dc2587b77f66176384d75516b0d5
Hwassef <hassine.wassef@outloook.com>
2022-11-01 13:16:27 -1100

    Initialize default text theme style for bodyText2"

--- lib/app/config/themes/dark_theme/app_dark_theme.dart
+++ lib/app/config/themes/dark_theme/app_dark_theme.dart
@@ -1,6 +1,7 @@
 import 'package:change_profile_avatar/app/config/themes/dark_theme/app_dialog_dark_theme.dart';
 import 'package:flutter/material.dart';
 
+import '../../colors/app_dark_mode_colors.dart';
 import 'app_modal_bottom_sheet_dark_theme.dart';
 
 class AppDarkTheme {
@@ -8,5 +9,10 @@ class AppDarkTheme {
   static ThemeData darkTheme = ThemeData(
     dialogTheme: AppDialogDarkTheme.dialogTheme,
     bottomSheetTheme: AppModalBottomSheetDarkTheme.bottomSheetThemeData,
+    textTheme: const TextTheme(
+        bodyText1: TextStyle(
+      fontSize: 14,
+      color: AppDarkModeColors.whiteColor,
+    )),
   );
 }
