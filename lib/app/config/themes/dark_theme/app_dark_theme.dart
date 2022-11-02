6a50e6c3ea29cc9d4b0631007442c1222f8ea79a
Hwassef <hassine.wassef@outloook.com>
2022-11-01 14:21:40 -1100

    Add different text themes && icon theme

--- lib/app/config/themes/dark_theme/app_dark_theme.dart
+++ lib/app/config/themes/dark_theme/app_dark_theme.dart
@@ -9,10 +9,71 @@ class AppDarkTheme {
   static ThemeData darkTheme = ThemeData(
     dialogTheme: AppDialogDarkTheme.dialogTheme,
     bottomSheetTheme: AppModalBottomSheetDarkTheme.bottomSheetThemeData,
-    textTheme: const TextTheme(
-        bodyText2: TextStyle(
-      fontSize: 14,
+    iconTheme: const IconThemeData(
       color: AppDarkModeColors.whiteColor,
-    )),
+      size: 28.0,
+    ),
+    textTheme: const TextTheme(
+      displayLarge: TextStyle(
+        fontSize: 14,
+        color: AppDarkModeColors.whiteColor,
+      ),
+      displayMedium: TextStyle(
+        fontSize: 14,
+        color: AppDarkModeColors.whiteColor,
+      ),
+      displaySmall: TextStyle(
+        fontSize: 14,
+        color: AppDarkModeColors.whiteColor,
+      ),
+      headlineLarge: TextStyle(
+        fontSize: 14,
+        color: AppDarkModeColors.whiteColor,
+      ),
+      headlineMedium: TextStyle(
+        fontSize: 14,
+        color: AppDarkModeColors.whiteColor,
+      ),
+      headlineSmall: TextStyle(
+        fontSize: 14,
+        color: AppDarkModeColors.whiteColor,
+      ),
+      titleLarge: TextStyle(
+        fontSize: 14,
+        color: AppDarkModeColors.whiteColor,
+      ),
+      titleMedium: TextStyle(
+        fontSize: 14,
+        color: AppDarkModeColors.whiteColor,
+      ),
+      titleSmall: TextStyle(
+        fontSize: 14,
+        color: AppDarkModeColors.whiteColor,
+      ),
+      bodyLarge: TextStyle(
+        fontSize: 18,
+        color: AppDarkModeColors.whiteColor,
+      ),
+      bodyMedium: TextStyle(
+        fontSize: 14,
+        color: AppDarkModeColors.whiteColor,
+      ),
+      bodySmall: TextStyle(
+        fontSize: 14,
+        color: AppDarkModeColors.whiteColor,
+      ),
+      labelLarge: TextStyle(
+        fontSize: 14,
+        color: AppDarkModeColors.whiteColor,
+      ),
+      labelMedium: TextStyle(
+        fontSize: 14,
+        color: AppDarkModeColors.whiteColor,
+      ),
+      labelSmall: TextStyle(
+        fontSize: 14,
+        color: AppDarkModeColors.whiteColor,
+      ),
+    ),
   );
 }
