9b4be6901223dbd6a702ed14667289b9244c4860
Hwassef <hassine.wassef@outloook.com>
2022-11-02 12:39:02 -1100

    Register MediaService as singleton

--- lib/injection_container.dart
+++ lib/injection_container.dart
@@ -1,3 +1,5 @@
+import 'package:change_profile_avatar/app/data/services/media_service.dart';
+import 'package:change_profile_avatar/app/data/services/media_service_implementation.dart';
 import 'package:get_it/get_it.dart';
 
 import 'app/data/services/permission/permission_handle_permission_service.dart';
@@ -7,5 +9,8 @@ final sl = GetIt.instance;
 Future<void> init() async {
   sl.registerSingleton<PermissionService>(
     PermissionHandlerPermissionService(),
+  );
+  sl.registerSingleton<MediaService>(
+    MediaServiceImplementation(),
   );
 }
