17789f0a24bbee19a05c7d8e8258b740bf711866
Hwassef <hassine.wassef@outloook.com>
2022-11-02 12:20:54 -1100

    Create home page state

--- lib/app/pages/home_page/bloc/home_page_state.dart
+++ lib/app/pages/home_page/bloc/home_page_state.dart
@@ -1,6 +1,24 @@
-abstract class HomePageEvent extends Equatable {
-  const HomePageEvent();
+part of 'home_page_bloc.dart';
+
+class HomePageState extends Equatable {
+  HomePageState({
+    this.profileImage,
+  });
+
+  final File? profileImage;
+
+  HomePageState copyWith({
+    File? profileImage,
+  }) {
+    return HomePageState(
+      profileImage: profileImage ?? this.profileImage,
+    );
+  }
 
   @override
-  List<Object?> get props => [];
+  List<Object?> get props => [
+        profileImage,
+      ];
 }
+
+class HomePageInitial extends HomePageState {}
