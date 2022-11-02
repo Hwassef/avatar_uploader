8618e05718475a0f40eaa071526679db8738c892
Hwassef <hassine.wassef@outloook.com>
2022-11-02 11:20:41 -1100

    Rename method name

--- lib/app/data/services/permission/permission_service.dart
+++ lib/app/data/services/permission/permission_service.dart
@@ -3,6 +3,6 @@ import 'package:flutter/cupertino.dart';
 abstract class PermissionService {
   Future requestPhotosPermission();
   Future<bool> handlePhotosPermission({required BuildContext context});
-  Future requestCamerapermission();
+  Future requestCameraPermission();
   Future<bool> handleCameraPermission({required BuildContext context});
 }
