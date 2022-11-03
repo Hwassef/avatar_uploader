43fca63edcc840f3aa73168e98bf39aa28b75117
Hwassef <hassine.wassef@outloook.com>
2022-11-02 23:09:58 -1100

    Show Alert Dialog on pop

--- lib/app/pages/home_page/home_page.dart
+++ lib/app/pages/home_page/home_page.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:get_it/get_it.dart';
 
+import '../../utils/principal_functions.dart';
 import 'bloc/home_page_bloc.dart';
 
 class HomePage extends StatelessWidget {
@@ -12,43 +13,47 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final HomePageBloc homePageBloc = GetIt.I<HomePageBloc>();
-    return Scaffold(
-      body: SafeArea(
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Padding(
-              padding: const EdgeInsets.only(top: 80.0),
-              child: BlocBuilder<HomePageBloc, HomePageState>(
-                builder: (context, state) {
-                  return CircleAvatar(
-                    radius: 58,
-                    backgroundImage: state.profileImage != null
-                        ? Image.file(state.profileImage!).image
-                        : const NetworkImage('https://cdn.icon-icons.com/icons2/1378/PNG/512/avatardefault_92824.png'),
-                    child: Stack(
-                      children: [
-                        GestureDetector(
-                          onTap: () => showModalBottomSheet(
-                            context: context,
-                            builder: (_) => const UpdateProfilePictureModalBottomSheet(),
-                          ),
-                          child: const Align(
-                            alignment: Alignment.bottomRight,
-                            child: CircleAvatar(
-                              radius: 18,
-                              backgroundColor: Colors.white70,
-                              child: Icon(CupertinoIcons.camera),
+    return WillPopScope(
+      onWillPop: () => onBackPressed(context),
+      child: Scaffold(
+        body: SafeArea(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(top: 80.0),
+                child: BlocBuilder<HomePageBloc, HomePageState>(
+                  builder: (context, state) {
+                    return CircleAvatar(
+                      radius: 58,
+                      backgroundImage: state.profileImage != null
+                          ? Image.file(state.profileImage!).image
+                          : const NetworkImage(
+                              'https://cdn.icon-icons.com/icons2/1378/PNG/512/avatardefault_92824.png'),
+                      child: Stack(
+                        children: [
+                          GestureDetector(
+                            onTap: () => showModalBottomSheet(
+                              context: context,
+                              builder: (_) => const UpdateProfilePictureModalBottomSheet(),
+                            ),
+                            child: const Align(
+                              alignment: Alignment.bottomRight,
+                              child: CircleAvatar(
+                                radius: 18,
+                                backgroundColor: Colors.white70,
+                                child: Icon(CupertinoIcons.camera),
+                              ),
                             ),
                           ),
-                        ),
-                      ],
-                    ),
-                  );
-                },
+                        ],
+                      ),
+                    );
+                  },
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
