1daaaea7233359e2803a056981c8b7600a46e15f
Hwassef <hassine.wassef@outloook.com>
2022-11-02 13:10:48 -1100

    Add HomePageBloc in BlocProvider

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'app/config/themes/dark_theme/app_dark_theme.dart';
 import 'app/config/themes/light_theme/app_light_theme.dart';
-import 'app/pages/home_page.dart';
+import 'app/pages/home_page/bloc/home_page_bloc.dart';
+import 'app/pages/home_page/home_page.dart';
 import 'injection_container.dart' as di;
+import 'injection_container.dart';
 
 void main() async {
   await di.init();
@@ -16,10 +19,13 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      darkTheme: AppDarkTheme.darkTheme,
-      theme: AppLightTheme.lightTheme,
-      home: const HomePage(),
+    return BlocProvider(
+      create: (context) => sl<HomePageBloc>(),
+      child: MaterialApp(
+        darkTheme: AppDarkTheme.darkTheme,
+        theme: AppLightTheme.lightTheme,
+        home: const HomePage(),
+      ),
     );
   }
 }
