ec6579e5c46cbf047a40a63bfc3651f48f47ef12
Hwassef <hassine.wassef@outloook.com>
2022-11-02 13:11:42 -1100

    Rebuild widget if profileImage exist

--- lib/app/pages/home_page/home_page.dart
+++ lib/app/pages/home_page/home_page.dart
@@ -1,12 +1,17 @@
-import 'package:change_profile_avatar/app/pages/widgets/update_profile_picture_modal_bottom_sheet.dart';
+import 'package:change_profile_avatar/app/pages/home_page/widgets/update_profile_picture_modal_bottom_sheet.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:get_it/get_it.dart';
+
+import 'bloc/home_page_bloc.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final HomePageBloc homePageBloc = GetIt.I<HomePageBloc>();
     return Scaffold(
       body: SafeArea(
         child: Row(
@@ -14,29 +19,33 @@ class HomePage extends StatelessWidget {
           children: [
             Padding(
               padding: const EdgeInsets.only(top: 80.0),
-              child: CircleAvatar(
-                radius: 58,
-                backgroundImage: const NetworkImage(
-                  "https://image.similarpng.com/thumbnail/2020/04/Instagram-logo-modern-paint-splash-social-media-png.png",
-                ),
-                child: Stack(
-                  children: [
-                    GestureDetector(
-                      onTap: () => showModalBottomSheet(
-                        context: context,
-                        builder: (_) => const UpdateProfilePictureModalBottomSheet(),
-                      ),
-                      child: const Align(
-                        alignment: Alignment.bottomRight,
-                        child: CircleAvatar(
-                          radius: 18,
-                          backgroundColor: Colors.white70,
-                          child: Icon(CupertinoIcons.camera),
+              child: BlocBuilder<HomePageBloc, HomePageState>(
+                builder: (context, state) {
+                  return CircleAvatar(
+                    radius: 58,
+                    backgroundImage: state.profileImage != null
+                        ? Image.file(state.profileImage!).image
+                        : const NetworkImage('https://cdn.icon-icons.com/icons2/1378/PNG/512/avatardefault_92824.png'),
+                    child: Stack(
+                      children: [
+                        GestureDetector(
+                          onTap: () => showModalBottomSheet(
+                            context: context,
+                            builder: (_) => const UpdateProfilePictureModalBottomSheet(),
+                          ),
+                          child: const Align(
+                            alignment: Alignment.bottomRight,
+                            child: CircleAvatar(
+                              radius: 18,
+                              backgroundColor: Colors.white70,
+                              child: Icon(CupertinoIcons.camera),
+                            ),
+                          ),
                         ),
-                      ),
+                      ],
                     ),
-                  ],
-                ),
+                  );
+                },
               ),
             ),
           ],
